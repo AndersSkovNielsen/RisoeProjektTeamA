a76ded059f4b7bf6d60dfff8403d4c40a0f53ed1
AndersSkovNielsen <anderssnielsen@live.dk>
2018-04-25 11:47:54 +0200

    Minor MainPage tinkering

--- RisoeProjektTeamA/View/MainPage.xaml.cs
+++ RisoeProjektTeamA/View/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RisoeProjektTeamA.View;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -25,6 +26,11 @@ namespace RisoeProjektTeamA
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Hovedmenu));
         }
     }
 }
