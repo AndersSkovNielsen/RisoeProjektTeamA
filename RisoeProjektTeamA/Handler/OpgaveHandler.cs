59571541b6d681de5af045feb43f7c099600dfb8
Frederik Sthen Hansen <fred305c@edu.easj.dk>
2018-04-25 14:22:58 +0200

    Udbygningning af Viewmodels og Handler

--- RisoeProjektTeamA/Handler/OpgaveHandler.cs
+++ RisoeProjektTeamA/Handler/OpgaveHandler.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RisoeProjektTeamA.Model;
+using RisoeProjektTeamA.ViewModel;
 
 namespace RisoeProjektTeamA.Handler
 {
     class OpgaveHandler
     {
+        private OpgaveViewModel opgaveViewModel;
+        private LogbogSingleton Logbog;
+
+        public OpgaveHandler(OpgaveViewModel opgaveViewModel, string beskrivelse)
+        {
+            this.opgaveViewModel = opgaveViewModel;
+            _beskrivelse = beskrivelse;
+            Logbog = LogbogSingleton.Instance(); // er dette det rigtige sted at contsruere vores LogbogSIngleton?
+        }
         private string _beskrivelse;
 
         public string Beskrivelse
