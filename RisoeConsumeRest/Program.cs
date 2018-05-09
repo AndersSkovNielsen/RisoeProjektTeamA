3dc06a54debe9f3798863ba3d565a71c3c28bd2f
Joachim Grubbe Frank <jgf.easj@gmail.com>
2018-05-09 12:28:15 +0200

    Tilføjet Rigtig ConsumeRest. Masse fiks.

---
+++ RisoeConsumeRest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RisoeConsumeRest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Her ligger ConsumeRest
+
+            ConsumeRest RestService = new ConsumeRest();
+            RestService.Test();
+            Console.Clear();
+            Console.WriteLine("Slut på Test");
+            Console.ReadKey();
+        }
+    }
+}
